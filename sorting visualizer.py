3e4966bee7018877070a3d88261e3bf0beb31ddd
Akash <57212963+Akash-152000@users.noreply.github.com>
2020-05-30 10:00:33 +0530

    Update sorting visualizer.py

--- sorting visualizer.py
+++ sorting visualizer.py
@@ -1,31 +1,27 @@
 import pygame
 import random
 
-screen=pygame.display.set_mode((900,650))
-pygame.display.set_caption("Sorting visuallizer")
-
 pygame.init()
 
+surface=pygame.display.set_mode((900,650))
+pygame.display.set_caption("Sorting visuallizer")
+
+
+
 gameLoop=True
-
-green=(0,204,102)
-red=(255,0,0)
-blue=(0,0,255)
-yellow=(255,255,0)
-
-
-
-array=[0]*151
-arr_clr=[green]*151
-color=[red,blue,yellow]
 
 while gameLoop:
     
     for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 gameLoop=False
+            pygame.draw.rect(surface,(255,255,255),[random.randrange(1,100),20,20,20])
 
-    surface.fill(black)
-    pygame.display.flip()
+    surface.fill((0,0,0))
+    pygame.draw.rect(surface,(255,255,255),[random.randrange(1,100),20,20,20])
+    
+    pygame.display.update()
 pygame.quit()
+
+
     
