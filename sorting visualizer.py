9e93d5daad8ba6706088bd187ebe75b7f31d4bf6
Akash <57212963+Akash-152000@users.noreply.github.com>
2020-05-30 20:29:19 +0530

    Update sorting visualizer.py

--- sorting visualizer.py
+++ sorting visualizer.py
@@ -2,7 +2,7 @@ import pygame
 import random
 pygame.init()
 
-surface=pygame.display.set_mode((900,700))
+surface=pygame.display.set_mode((1000,700))
 pygame.display.set_caption("Sorting visualizer")
 
 #Colours
@@ -10,9 +10,10 @@ red=(255,99,71)
 green=(0,255,0)
 blue=(0,0,255)
 black=(0,0,0)
+orange=(255,165,0)
 white=(255,255,255)
 grey=(211,211,211)
-
+color_array=[orange]*200
 array=[0]*200
 
 def generate_array():
@@ -26,21 +27,30 @@ generate_array()
 def draw_lines():
     #Horizontal grey lines
     for i in range(1,150):
-        pygame.draw.line(surface,grey,(0,5*i),(900,5*i),1)
+        pygame.draw.line(surface,grey,(0,5*i),(1000,5*i),1)
 
     for i in range(1,200):
-        pygame.draw.line(surface,red,(5*i,6),(5*i,array[i]*3),3)
-    pygame.draw.line(surface,black,(0,0),(900,0),10)
+        pygame.draw.line(surface,color_array[i],(5*i,6),(5*i,array[i]*3),3)
+    pygame.draw.line(surface,black,(0,0),(1000,0),10)
 
 ##Sorting algorithm
 def bubbleSort(arr):
     for i in range(len(arr)):
         for j in range(i, len(arr)):
             if arr[i]>arr[j]:
+                color_array[i]=blue
+                color_array[j]=black
                 arr[i],arr[j]=arr[j],arr[i]
+                surface.fill(white)
                 draw_lines()
+                pygame.display.update()
+                color_array[i]=green
+                color_array[j]=orange
+        color_array[i]=green
 
+def selectionSort(arr):
     
+
 gameLoop=True
 while gameLoop:
     surface.fill(white)
@@ -50,11 +60,11 @@ while gameLoop:
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_UP:
                 bubbleSort(array)
-
+            if event.key==pygame.K_DOWN:
+                selectionSort(array)
     draw_lines()
     pygame.display.update()
 pygame.quit()
-
 
 
 
