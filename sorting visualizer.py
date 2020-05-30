27b65a834149422c1d504bbacd0452737a4450fb
Akash <57212963+Akash-152000@users.noreply.github.com>
2020-05-30 14:56:27 +0530

    Update sorting visualizer.py

--- sorting visualizer.py
+++ sorting visualizer.py
@@ -8,6 +8,31 @@ pygame.display.set_caption("Sorting visuallizer")
 
 
 
+#colors
+red=(255,0,0)
+blue=(0,0,255)
+green=(0,255,0)
+white=(255,255,255)
+black=(0,0,0)
+x=10
+y=10
+array=[0]*151
+def arr_generate():
+    for i in range(1,151):
+        array[i]=random.randrange(1,151)
+arr_generate()
+
+def draw():
+    element_width =(900-150)//150
+    pygame.draw.line(surface, (0, 0, 0),  (0, 95), (900, 95), 6) 
+    for i in range(1, 100): 
+        pygame.draw.line(surface,black,(0, 5* i + 100),(900, 5* i + 100), 1)
+    
+    for  i in range(1, 151): 
+        pygame.draw.line(surface, red,(6 * i-3, 100),(6* i-3, array[i]*5 + 100),element_width) 
+
+
+    
 gameLoop=True
 
 while gameLoop:
@@ -15,13 +40,11 @@ while gameLoop:
     for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 gameLoop=False
-            pygame.draw.rect(surface,(255,255,255),[random.randrange(1,100),20,20,20])
-
-    surface.fill((0,0,0))
-    pygame.draw.rect(surface,(255,255,255),[random.randrange(1,100),20,20,20])
-    
+    surface.fill(white)
+    draw()
+    y+=1
     pygame.display.update()
 pygame.quit()
 
 
-    
+
